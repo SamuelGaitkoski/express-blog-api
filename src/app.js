c500a619ddf4bd43cdb41b09ece7fef7d6cac62c
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-24 14:58:10 -0300

    Set up Express app and server entry point.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import express from "express";
-import postsRouter from "./routes/posts.js";
 import logger from "./middlewares/logger.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import postsRouter from "./routes/posts.js";
 
 const app = express();
 
 // Middlewares
+// Built-in middleware to parse JSON request bodies
 app.use(express.json());
+// Our logger
 app.use(logger);
+
+// Health check
+app.get("/health", (req, res) => res.json({ ok: true }));
 
 // Routes
 app.use("/posts", postsRouter);
+
+// 404 for unknown routes
+app.use((req, res) => res.status(404).json({ error: "Not found" }));
 
 // Error handler
 app.use(errorHandler);
