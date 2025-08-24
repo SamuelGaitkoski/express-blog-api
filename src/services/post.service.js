2b99def1904071b04e394c2bb8e195ed700b0933
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-24 18:21:03 -0300

    Use lean for read methods and add runValidators to update in PostService.

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,21 +1,21 @@
 import Post from "../models/post.model";
 
 class PostService {
+  async getAll() {
+    return await Post.find().sort({ createdAt: -1 }).lean();
+  }
+
+  async getById(id) {
+    return await Post.findById(id).lean();
+  }
+
   async create(data) {
     const post = new Post(data);
     return await post.save();
   }
 
-  async getAll() {
-    return await Post.find();
-  }
-
-  async getById(id) {
-    return await Post.findById(id).len;
-  }
-
   async update(id, data) {
-    return await Post.findByIdAndUpdate(id, data, { new: true });
+    return await Post.findByIdAndUpdate(id, data, { new: true, runValidators: true });
   }
 
   async delete(id) {
