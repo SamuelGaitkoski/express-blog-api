ad7396a667d3caecef2582196952040710c37847
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-25 11:01:49 -0300

    Rename middlewares and utils files to solid naming standard.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/async-handler.util.js";
 import { AuthController } from "../controllers/auth.controller.js";
 
 const router = Router();
