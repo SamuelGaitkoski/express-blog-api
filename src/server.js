fb6170692459479231657d28e2aa8c09e9e3e627
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-24 21:17:22 -0300

    Add db.js and update server.js to connect to MongoDB database.

--- src/server.js
+++ src/server.js
@@ -3,9 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
+import { connectDB } from "./storage/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`API running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+};
+
