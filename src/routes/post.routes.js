990246dc455b126252a0864e476ce347ee800de8
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-25 11:39:54 -0300

    Integrate auth middleware into Post routes.

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,15 +1,46 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/async-handler.util.js";
+import { authenticateToken, authorizeRoles } from "../middlewares/auth.middleware.js";
+import { UserRole } from "../enums/user-role.enum.js";
 import PostControllerClass from "../controllers/post.controller.js";
 
 const router = Router();
 
 const PostController = new PostControllerClass();
 
-router.get("/", asyncHandler(PostController.getAll.bind(PostController)));
-router.get("/:id", asyncHandler(PostController.getById.bind(PostController)));
-router.post("/", asyncHandler(PostController.create.bind(PostController)));
-router.put("/:id", asyncHandler(PostController.update.bind(PostController)));
-router.delete("/:id", asyncHandler(PostController.delete.bind(PostController)));
+router.get(
+  "/",
+  authenticateToken,
+  authorizeRoles(UserRole.ADMIN),
+  asyncHandler(PostController.getAll.bind(PostController))
+);
+
+router.get(
+  "/:id",
+  authenticateToken,
+  authorizeRoles(UserRole.ADMIN),
+  asyncHandler(PostController.getById.bind(PostController))
+);
+
+router.post(
+  "/",
+  authenticateToken,
+  authorizeRoles(UserRole.USER),
+  asyncHandler(PostController.create.bind(PostController))
+);
+
+router.put(
+  "/:id",
+  authenticateToken,
+  authorizeRoles(UserRole.USER),
+  asyncHandler(PostController.update.bind(PostController))
+);
+
+router.delete(
+  "/:id",
+  authenticateToken,
+  authorizeRoles(UserRole.USER),
+  asyncHandler(PostController.delete.bind(PostController))
+);
 
 export default router;
