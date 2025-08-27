f3f5c57ecfae842974a6c30302bcb111d3c30b53
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-27 15:58:24 -0300

    Change Post model by replacing author with authorId to store userId when creating posts.

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -3,16 +3,17 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema(
   {
     title: { 
-      type: String, 
-      required: true 
+      type: String,
+      required: true
     },
     content: { 
-      type: String, 
-      required: true 
+      type: String,
+      required: true
     },
-    author: { 
-      type: String, 
-      required: true 
+    authorId: { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
     },
   },
   {
