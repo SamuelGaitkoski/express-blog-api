af89ec28aaf621f5fbc4753662699846b69307a2
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-27 17:26:58 -0300

    Add validation for fullName, email, and password in AuthController register method.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,10 @@ export class AuthController {
   static async register(req, res) {
     const { fullName, email, password, adminCode } = req.body;
 
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ error: "FullName, Email and Password are required" });
+    }
+
     const user = await authService.register(fullName, email, password, adminCode);
 
     return res.status(201).json({ message: "User registered successfully", user });
